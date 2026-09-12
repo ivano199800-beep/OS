2382a40f795b80d10a8dc0df835cf362795ff8a6
ivan <ivano199800@gmail.com>
2026-09-13 00:21:19 +0800

    finished initial layout

---
+++ layout.s
@@ -0,0 +1,17 @@
+format binary
+; bootloader
+file "boot.bin"
+; bios utilities
+file "util.bin"
+; paddinf
+times 1024 - ($-$$) db 0
+; nesessicary utils 
+file "util32.bin"
+times 2048 - ($-$$) db 0
+file "fsutil.bin"
+times 4096 - ($-$$) db 0
+; first stage kernel (interupt dispatch table setup) , and loading the filesystem to launch 0/init.bin
+file "kernel.bin"
+times (4096 + 1024) - ($-$$) db 0
+; filesystem first sector is the root table
+file "filesystem.bin"
