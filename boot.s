68a9daaa560369fae93ee64ce7eec824ee4476cf
ivan <ivano199800@gmail.com>
2026-09-13 13:02:03 +0800

    made a simple boot program

---
+++ boot.s
@@ -0,0 +1,46 @@
+org 0x7c00
+jmp start_
+; Data
+text: db "BOOT TEXT 16BIT" , 0 
+; function (cdecl)16bit
+putc16: ; ret -> char
+  push bx
+  mov bx , sp
+  add bx , 4
+  mov al , [bx]
+  mov ah , 0x0e
+  int 10h
+  pop bx
+  ret
+puts16:
+  push bx
+  mov bx , sp
+  add bx , 4 
+  mov si , [bx]
+  .loop:
+  mov al , [si]
+  test al , al
+  jz .end
+  inc si
+  push ax
+  call putc16
+  pop ax
+  jmp .loop
+  .end:
+  pop bx
+  ret
+start_:
+  xor ax , ax
+  cli 
+  mov bp , sp
+  sti
+  push text
+  call puts16
+  add sp , 2
+hang_:
+  cli 
+  .loop:
+  hlt 
+  jmp .loop
+times 510 - ($-$$) db 0
+dw 0xAA55
